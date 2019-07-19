406b5fe3cfb7b311a84a9b41af0633a015992864
Adam Bertus <bertus.rocky@gmail.com>
2019-07-19 15:55:40 +0100

    final version before sass

--- app.js
+++ app.js
@@ -1,1 +1,70 @@
-// ready for new stuff
+// Animate elements with ScrollReveal
+
+window.sr = ScrollReveal();
+sr.reveal('.mainLeft', {
+  duration: 2000,
+  });
+
+sr.reveal('#phone', {
+  duration: 2000,
+  origin: 'right',
+  distance: '200px'
+  });
+
+sr.reveal('.mainButtons', {
+  duration: 1500,
+  delay: 1500,
+  origin: 'bottom',
+  distance: '60px'
+  });
+
+sr.reveal('.headingStat', {
+  duration: 2300,
+  viewFactor: 1
+  });
+
+sr.reveal('.statUser', {
+  duration: 1500,
+  viewFactor: 1,
+  origin: 'bottom',
+  distance: '20px'
+  });
+  
+sr.reveal('.statLike', {
+  duration: 1500,
+  delay:300,
+  viewFactor: 1,
+  origin: 'bottom',
+  distance: '20px'
+  });
+
+sr.reveal('.statReview', {
+  duration: 1500,
+  viewFactor: 1,
+  delay:600,
+  origin: 'bottom',
+  distance: '20px'
+  });
+
+sr.reveal('.statUsability', {
+  duration: 1500,
+  delay:800,
+  viewFactor: 1,
+  origin: 'bottom',
+  distance: '20px'
+  });
+
+sr.reveal('.watchFlexRight', {
+  duration: 2500,
+  viewFactor: 0.4,
+  origin: 'right',
+  distance: '200px'
+  });
+
+sr.reveal('.formColumn', {
+  duration: 2000,
+  viewFactor: 0.3,
+  origin: 'bottom',
+  distance: '30px'
+  });
+  
